c45a133f06bd4cadf48bea6277ea64eb43700865
Sapti Sunil <sapti.s@innoneur.com>
2022-03-24 18:09:52 +0530

    commit 6

--- components/device-table/deviceTable.php
+++ components/device-table/deviceTable.php
@@ -52,16 +52,3 @@
   </div>
   </div>
 </div>
-
-<script>
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == document.getElementById("myModal")) {
-    document.getElementById("myModal").style.display = "none";
-  }
-}
-function openModal(){
-  document.getElementById('myModal').style.display = "block";
-}
-</script>
--- routes/device.php
+++ routes/device.php
@@ -8,7 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../assets/styles/styles.css">
-  <link rel="stylesheet" href="mystyles.css">
+  <link rel="stylesheet" href="../assets/styles/mystyles.css">
   <script src="https://use.fontawesome.com/9608db71df.js"></script>
   <title>Falcontrac</title>
 </head>
@@ -31,7 +31,7 @@
       ?>
     </div>
   </div>
-
+  <script src="../assets/js/device.js"></script>
 </body>
 
 </html>
